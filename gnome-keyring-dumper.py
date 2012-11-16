667dfc5439cfef4789237cf39704740a9b5be59e
Michael Nagel <michael.nagel@devzero.de>
2012-11-16 23:42:49 +0100

    print usernames if available

--- gnome-keyring-dumper.py
+++ gnome-keyring-dumper.py
@@ -11,8 +11,19 @@ def hack():
     for keyring in gnomekeyring.list_keyring_names_sync():
         for id in gnomekeyring.list_item_ids_sync(keyring):
             item = gnomekeyring.item_get_info_sync(keyring, id)
-            print '[%s] %s = %s' % (
-                    keyring, item.get_display_name(), item.get_secret()
+            attr = gnomekeyring.item_get_attributes_sync(keyring, id)
+            if attr and attr.has_key('username_value'):
+                print '[%s] %s: %s = %s' % (
+                    keyring,
+                    item.get_display_name(),
+                    attr['username_value'],
+                    item.get_secret()
+                )
+            else:
+                print '[%s] %s = %s' % (
+                    keyring,
+                    item.get_display_name(),
+                    item.get_secret()
             )
         else:
             if len(gnomekeyring.list_item_ids_sync(keyring)) == 0:
