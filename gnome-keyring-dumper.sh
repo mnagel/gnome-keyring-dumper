d0c6ff474196f28364d8224afefc11a7f2631316
Michael Nagel <michael.nagel@devzero.de>
2012-11-16 23:43:16 +0100

    add bash script to launch over ssh

---
+++ gnome-keyring-dumper.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+eval $(dbus-launch "--sh-syntax")
+eval $(/usr/bin/gnome-keyring-daemon "--start" "--components=gpg,pkcs11,secrets,ssh")
+export $(gnome-keyring-daemon)
+
+python -c "import gnomekeyring;import getpass;p=getpass.getpass();gnomekeyring.unlock_sync(None,p);"
+
+python gnome-keyring-dumper.py
