dd6ad84efd38686ecef3073b5d5b59eeb96e2bde
antitree <antitree@gmail.com>
2017-04-19 11:20:42 -0400

    fixing unlock issue. Now automatically unlocks before trying to read the keyring

--- gnome-keyring-dumper-ng.py
+++ gnome-keyring-dumper-ng.py
@@ -8,6 +8,8 @@ def hackng():
     bus = secretstorage.dbus_init()
     for keyring in secretstorage.get_all_collections(bus):
         for item in keyring.get_all_items():
+            if item.is_locked(): 
+                item.unlock()
             attr = item.get_attributes()
             if attr and 'username_value' in attr:
                 print('[%s] %s: %s = %s' % (
@@ -21,7 +23,7 @@ def hackng():
                     keyring.get_label(),
                     item.get_label(),
                     item.get_secret()
-            ))
+                ))
 
 if __name__ == '__main__':
     hackng()
